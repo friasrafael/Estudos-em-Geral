e91791870bfbd6e773075dd865d04c6a39867554
friasrafael <rafaelfriasdacosta@gmail.com>
2021-03-13 23:46:43 -0300

    Ajustes

--- Projetos Pessoais/Calculadora de IMC JavaScript/treinando.js
+++
@@ -1,8 +0,0 @@
-function update() {
-    var select = document.getElementById('objetivo');
-    var option = select.options[select.selectedIndex];
-
-    
-    document.getElementById('value').value = option.value;
-}
-update();
